487d0e1b77b3fa4e6901980b5ab1397dcea75fed
Namaara Racheal <namaara.racheal@lenoxparkinc.com>
2022-06-20 14:55:34 +0300

    Fix back button bug when voting

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Exceptions\DuplicateVoteException;
+use App\Exceptions\VoteNotFoundException;
 
 class Idea extends Model
 {
@@ -59,6 +61,9 @@ class Idea extends Model
 
     public function vote(User $user)
     {
+        if ($this->isVotedByUser($user)) {
+            throw new DuplicateVoteException;
+        }
         Vote::create([
             'idea_id' => $this->id,
             'user_id' => $user->id,
@@ -67,10 +72,15 @@ class Idea extends Model
 
     public function removeVote(User $user)
     {
-        Vote::where('idea_id', $this->id)
+        $voteToDelete = Vote::where('idea_id', $this->id)
             ->where('user_id', $user->id)
-            ->first()
-            ->delete();
+            ->first();
+
+        if ($voteToDelete) {
+            $voteToDelete->delete();
+        } else {
+            throw new VoteNotFoundException;
+        }
     }
 
 
