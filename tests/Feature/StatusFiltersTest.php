e2ccfdc5cd60f08230c506951fb46b1c1402d73d
Namaara Racheal <namaara.racheal@lenoxparkinc.com>
2022-06-20 23:08:24 +0300

    Category Filters tests

--- tests/Feature/StatusFiltersTest.php
+++ tests/Feature/StatusFiltersTest.php
@@ -2,11 +2,12 @@
 
 namespace Tests\Feature;
 
-use App\Http\Livewire\StatusFilters;
-use App\Models\Category;
 use App\Models\Idea;
-use App\Models\Status;
 use App\Models\User;
+use App\Models\Status;
+use App\Models\Category;
+use App\Http\Livewire\IdeasIndex;
+use App\Http\Livewire\StatusFilters;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Livewire\Livewire;
@@ -131,10 +132,12 @@ class StatusFiltersTest extends TestCase
             'status_id' => $statusInProgress->id,
         ]);
 
-        $response = $this->get(route('idea.index', ['status' => 'In Progress']));
-        $response->assertSuccessful();
-        $response->assertSee('<div class="bg-yellow text-white text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">In Progress</div>', false);
-        $response->assertDontSee('<div class="bg-purple text-white text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">Considering</div>', false);
+        Livewire::withQueryParams(['status' => 'In Progress'])
+            ->test(IdeasIndex::class)
+            ->assertViewHas('ideas', function ($ideas) {
+                return $ideas->count() === 3
+                    && $ideas->first()->status->name === 'In Progress';
+            });
     }
 
     /** @test */
@@ -168,31 +171,30 @@ class StatusFiltersTest extends TestCase
     }
 
     /** @test */
-    public function index_page_shows_selected_status()
-    {
-        $user = User::factory()->create();
-
-        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
-
-        $statusImplemented = Status::factory()->create(['id' => 4, 'name' => 'Implemented']);
-
-        Idea::factory()->create([
-            'user_id' => $user->id,
-            'category_id' => $categoryOne->id,
-            'status_id' => $statusImplemented->id,
-            'title' => 'My First Idea',
-            'description' => 'Description for my first idea',
-        ]);
-
-        $idea = Idea::factory()->create([
-            'user_id' => $user->id,
-            'category_id' => $categoryOne->id,
-            'status_id' => $statusImplemented->id,
-            'title' => 'My First Idea',
-            'description' => 'Description for my first idea',
-        ]);
-
-        $response = $this->get(route('idea.index'));
-//        $response->assertSee('border-blue text-gray-900');
-    }
+//    public function index_page_shows_selected_status()
+//    {
+//        $user = User::factory()->create();
+//
+//        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+//
+//        $statusImplemented = Status::factory()->create(['id' => 4, 'name' => 'Implemented']);
+//
+//        Idea::factory()->create([
+//            'user_id' => $user->id,
+//            'category_id' => $categoryOne->id,
+//            'status_id' => $statusImplemented->id,
+//            'title' => 'My First Idea',
+//            'description' => 'Description for my first idea',
+//        ]);
+//
+//        $idea = Idea::factory()->create([
+//            'user_id' => $user->id,
+//            'category_id' => $categoryOne->id,
+//            'status_id' => $statusImplemented->id,
+//            'title' => 'My First Idea',
+//            'description' => 'Description for my first idea',
+//        ]);
+//
+//        $response = $this->get(route('idea.index'));
+//    }
 }
