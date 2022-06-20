afac596406fa553ada85acad42b270872506bcc7
Namaara Racheal <namaara.racheal@lenoxparkinc.com>
2022-06-20 23:54:07 +0300

    back button with filters

--- app/Http/Livewire/IdeasIndex.php
+++ app/Http/Livewire/IdeasIndex.php
@@ -14,10 +14,14 @@ class IdeasIndex extends Component
     use WithPagination;
 
     public $status;
+    public $filter;
+    public $search;
     public $category;
 
     protected $queryString = [
         'status',
+        'filter',
+        'search',
         'category',
     ];
 
@@ -26,6 +30,24 @@ class IdeasIndex extends Component
     public function mount()
     {
         $this->status = request()->status ?? 'All';
+    }
+    public function updatingFilter()
+    {
+        $this->resetPage();
+    }
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+
+    public function updatedFilter()
+    {
+        if ($this->filter === 'My Ideas') {
+            if (! auth()->check()) {
+                return redirect()->route('login');
+            }
+        }
     }
 
     public function updatingCategory()
@@ -50,6 +72,12 @@ class IdeasIndex extends Component
                     return $query->where('status_id', $statuses->get($this->status));
                 })->when($this->category && $this->category !== 'All Categories', function ($query) use ($categories) {
                     return $query->where('category_id', $categories->pluck('id', 'name')->get($this->category));
+                })->when($this->filter && $this->filter === 'Top Voted', function ($query) {
+                    return $query->orderByDesc('votes_count');
+                })->when($this->filter && $this->filter === 'My Ideas', function ($query) {
+                    return $query->where('user_id', auth()->id());
+                })->when(strlen($this->search) >= 3, function ($query) {
+                    return $query->where('title', 'like', '%'.$this->search.'%');
                 })
                 ->addSelect(['voted_by_user' => Vote::select('id')
                     ->where('user_id', auth()->id())
