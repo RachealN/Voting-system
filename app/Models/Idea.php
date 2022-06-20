6fc9b0b1d3440f12ac610ecd9cef6e7da478d059
Namaara Racheal <namaara.racheal@lenoxparkinc.com>
2022-06-20 14:44:32 +0300

    voting for ideas

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -57,6 +57,23 @@ class Idea extends Model
             ->exists();
     }
 
+    public function vote(User $user)
+    {
+        Vote::create([
+            'idea_id' => $this->id,
+            'user_id' => $user->id,
+        ]);
+    }
+
+    public function removeVote(User $user)
+    {
+        Vote::where('idea_id', $this->id)
+            ->where('user_id', $user->id)
+            ->first()
+            ->delete();
+    }
+
+
     public function getStatusClasses()
     {
         $all_statuses = [
